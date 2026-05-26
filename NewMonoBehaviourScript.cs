d8982ff1a0e05744dad0390f3ad82ba388157308
Justin-Herman <jstnhrmn06@gmail.com>
2026-05-26 16:04:04 +0200

    Add files via upload

---
+++ NewMonoBehaviourScript.cs
@@ -0,0 +1,42 @@
+using System.Diagnostics;
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    public int playerspeed;
+    public int enemyspeed;
+
+    public int enemyhealth;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (playerspeed == 0) 
+        {
+            if (playerspeed > enemyspeed) 
+               print("player moves first");
+        }
+        else
+        {
+            if (enemyspeed > playerspeed) 
+                print("enemy moves first");
+        }
+
+        int enemyhealth = 100;
+
+        if (Input.GetKey(KeyCode.E))
+        {
+           print(enemyhealth = 100 - 15);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+           print("Enemy has taken damage");
+        }
+    }
+}
